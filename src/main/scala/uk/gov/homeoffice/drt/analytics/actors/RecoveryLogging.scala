b2721f8ac82e93edd2f8ed427fdc251acfc9481f
Rich Birch <rich@freshcodelimited.com>
2020-07-09 11:27:59 +0100

    DRTII-517 Snapshot and truncate old data

---
+++ src/main/scala/uk/gov/homeoffice/drt/analytics/actors/RecoveryLogging.scala
@@ -0,0 +1,32 @@
+package uk.gov.homeoffice.drt.analytics.actors
+
+import akka.persistence.SnapshotMetadata
+import org.slf4j.Logger
+import uk.gov.homeoffice.drt.analytics.time.SDate
+
+
+trait RecoveryLogging {
+  val log: Logger
+
+  val prefix = "Recovery"
+
+  def snapshotOfferLogMessage(md: SnapshotMetadata): String = s"$prefix received SnapshotOffer from ${SDate(md.timestamp).toISOString}, sequence number ${md.sequenceNr}"
+
+  def logSnapshotOffer(md: SnapshotMetadata): Unit = log.info(snapshotOfferLogMessage(md))
+
+  def logSnapshotOffer(md: SnapshotMetadata,
+                       additionalInfo: String): Unit = log.info(s"${snapshotOfferLogMessage(md)} - $additionalInfo")
+
+  def logRecoveryMessage(message: String): Unit = log.info(s"$prefix - $message")
+
+  def logPointInTimeCompleted(pit: SDate): Unit = log.info(s"$prefix completed to point-in-time ${pit.toISOString}")
+
+  def logUnknown(unknown: Any): Unit = log.warn(s"$prefix received unknown message ${unknown.getClass}")
+
+  def logCounters(bytes: Int, messages: Int, bytesThreshold: Int, maybeMessageThreshold: Option[Int]): Unit = {
+    val megaBytes = bytes.toDouble / (1024 * 1024)
+    val megaBytesThreshold = bytesThreshold.toDouble / (1024 * 1024)
+    log.info(f"$megaBytes%.2fMB persisted in $messages messages since last snapshot. Thresholds: $megaBytesThreshold%.2fMB, $maybeMessageThreshold messages")
+  }
+}
+
