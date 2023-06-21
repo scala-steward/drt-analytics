15169be24e8be9a59d3a0e28a7f65ba94901fee9
Rich Birch <ringo@albumsnaps.com>
2023-06-21 14:05:55 +0100

    DRTII-1122 Update drt-lib. Make stats compilation more robust

--- src/main/scala/uk/gov/homeoffice/drt/analytics/services/ModelAccuracy.scala
+++ src/main/scala/uk/gov/homeoffice/drt/analytics/services/ModelAccuracy.scala
@@ -11,14 +11,15 @@ import uk.gov.homeoffice.drt.analytics.prediction.modeldefinitions.PaxModelStats
 import uk.gov.homeoffice.drt.analytics.s3.Utils
 import uk.gov.homeoffice.drt.analytics.services.ArrivalsHelper.populateMaxPax
 import uk.gov.homeoffice.drt.arrivals.Arrival
-import uk.gov.homeoffice.drt.ports.{ApiFeedSource, LiveFeedSource}
+import uk.gov.homeoffice.drt.ports.{ApiFeedSource, LiveFeedSource, MlFeedSource}
 import uk.gov.homeoffice.drt.ports.Terminals.Terminal
 import uk.gov.homeoffice.drt.prediction.ModelAndFeatures
-import uk.gov.homeoffice.drt.prediction.arrival.ArrivalModelAndFeatures
+import uk.gov.homeoffice.drt.prediction.arrival.{ArrivalModelAndFeatures, PaxCapModelAndFeatures}
 import uk.gov.homeoffice.drt.prediction.persistence.Flight
 import uk.gov.homeoffice.drt.time.{SDate, SDateLike, UtcDate}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
 object ModelAccuracy {
   private val log: Logger = LoggerFactory.getLogger(getClass)
@@ -44,7 +45,7 @@ object ModelAccuracy {
     Source(terminals.toList)
       .mapAsync(1) { terminal =>
         val terminalId = PredictionModelActor.Terminal(terminal.toString)
-        persistence.getModels(terminalId).map(models => (terminal, models))
+        persistence.getModels(Seq(PaxCapModelAndFeatures.targetName))(terminalId).map(models => (terminal, models))
       }
       .map { case (terminal, models) =>
         val modelsAndFeatures = collector(models.models.values)
@@ -57,9 +58,7 @@ object ModelAccuracy {
       }
       .mapAsync(1) { case (terminal, model) =>
         Source((0 until days).toList)
-          .mapAsync(1) { day =>
-            statsForDate(statsHelper, startDate, terminal, model, day)
-          }
+          .mapAsync(1)(day => statsForDate(statsHelper, startDate, terminal, model, day))
           .collect { case (date, predPax, actPax, fcstPax, flightsCount, predPctCap, actPctCap, fcstPctCap) if flightsCount > 0 =>
             val predDiff = (predPax - actPax).toDouble / actPax * 100
             val fcstDiff = (fcstPax - actPax).toDouble / actPax * 100
@@ -78,6 +77,11 @@ object ModelAccuracy {
           val csvContent = (csvHeader :: results.map(_._4).toList).mkString("\n")
           Utils.writeToBucket(bucketName, s"analytics/passenger-forecast/$port-$terminal.csv", csvContent)
             .map(_ => (terminal, results))
+            .recover {
+              case t: Throwable =>
+                log.error(s"Failed to write to bucket", t.getMessage)
+                (terminal, results)
+            }
       }
       .runForeach { case (terminal, results) => logStats(terminal, results) }
   }
@@ -93,22 +97,45 @@ object ModelAccuracy {
                             timeout: Timeout
                           ): Future[(UtcDate, Int, Int, Int, Int, Double, Double, Double)] = {
     val date = startDate.addDays(day).toUtcDate
-    val predFn: Arrival => Int = stats.predictionForArrival(model)
-
-    stats.arrivalsForDate(date, terminal, populateMaxPax, expectedFeeds = List(ApiFeedSource, LiveFeedSource)).map(_.filter(!_.Origin.isDomesticOrCta)).flatMap {
-      arrivals =>
-        stats.arrivalsForDate(date, terminal, populateMaxPax, Option(7), List()).map(_.filter(!_.Origin.isDomesticOrCta)).map {
-          fArrivals =>
-            val predPax = stats.sumPredPaxForDate(arrivals, predFn)
-            val actPax = stats.sumActPaxForDate(arrivals)
-            val fcstPax = stats.sumActPaxForDate(fArrivals)
-            val predPctCap = stats.sumPredPctCapForDate(arrivals, predFn)
-            val actPctCap = stats.sumActPctCapForDate(arrivals)
-            val fcstPctCap = stats.sumActPctCapForDate(fArrivals)
-            val flightsCount = arrivals.length
-            (date, predPax, actPax, fcstPax, flightsCount, predPctCap, actPctCap, fcstPctCap)
+    val predFn: Arrival => Int = arrival =>
+      model
+        .updatePrediction(arrival, 0, Option(100), SDate.now())
+        .PassengerSources.get(MlFeedSource).flatMap(_.actual)
+        .getOrElse {
+          println(s"Failed to get prediction for $arrival. Using 175")
+          175
         }
-    }
+
+    stats.arrivalsForDate(date, terminal, populateMaxPax, expectedFeeds = List(ApiFeedSource, LiveFeedSource))
+      .map(_.filter(!_.Origin.isDomesticOrCta))
+      .flatMap {
+        arrivals =>
+          stats.arrivalsForDate(date, terminal, populateMaxPax, Option(7), List())
+            .map(_.filter(!_.Origin.isDomesticOrCta))
+            .map {
+              fArrivals =>
+                val liveUniques = arrivals.map(_.unique)
+                val fUniques = fArrivals.map(_.unique)
+                val liveArrivals = arrivals.filter(a => fUniques.contains(a.unique))
+                val forecastArrivals = fArrivals.filter(a => liveUniques.contains(a.unique))
+                if (liveArrivals.length != forecastArrivals.length) {
+                  log.error(s"Got ${liveArrivals.length} liveArrivals and ${forecastArrivals.length} fcst arrivals for $date. Skipping")
+                  None
+                } else {
+                  val predPax = stats.sumPredPaxForDate(liveArrivals, predFn)
+                  val actPax = stats.sumActPaxForDate(liveArrivals)
+                  val fcstPax = stats.sumActPaxForDate(forecastArrivals)
+                  val predPctCap = stats.sumPredPctCapForDate(liveArrivals, predFn)
+                  val actPctCap = stats.sumActPctCapForDate(liveArrivals)
+                  val fcstPctCap = stats.sumActPctCapForDate(forecastArrivals)
+                  val flightsCount = liveArrivals.length
+                  Option((date, predPax, actPax, fcstPax, flightsCount, predPctCap, actPctCap, fcstPctCap))
+                }
+            }
+            .collect {
+              case Some(stats) => stats
+            }
+      }
   }
 
   private def logStats(terminal: Terminal, results: Seq[(Int, Int, Int, String)]): Unit = {
@@ -133,9 +160,24 @@ object ModelAccuracy {
     }
     val min = (minDiff._1 - minDiff._2).toDouble / minDiff._2 * 100
     val max = (maxDiff._1 - maxDiff._2).toDouble / maxDiff._2 * 100
-    val rmse = Math.sqrt(diffs.map(d => d * d).sum / diffs.length)
-    val meanPax = results.map(_._2).sum / results.length
-    val rmsePercent = rmse / meanPax * 100
+    val rmse = Try(Math.sqrt(diffs.map(d => (d * d).toLong).sum / diffs.length)) match {
+      case Success(r) => r
+      case Failure(t) =>
+        log.error(s"Failed to calculate RMSE", t)
+        0.0
+    }
+    val meanPax = Try(results.map(_._2).sum / results.length) match {
+      case Success(r) => r
+      case Failure(t) =>
+        log.error(s"Failed to calculate mean pax", t)
+        0
+    }
+    val rmsePercent = Try(rmse / meanPax * 100) match {
+      case Success(r) => r
+      case Failure(t) =>
+        log.error(s"Failed to calculate RMSE percent: $rmse, $meanPax", t)
+        0.0
+    }
     (min, max, meanPax, rmsePercent)
   }
 }
