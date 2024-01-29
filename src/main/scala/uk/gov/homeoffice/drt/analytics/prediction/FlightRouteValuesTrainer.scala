29a10d8469de617007c20b122b8c9290dbd4c2ec
Rich Birch <ringo@albumsnaps.com>
2024-01-29 10:10:12 +0000

    DRTII-1346 See if suspending outlier removal resolves indexing error

--- src/main/scala/uk/gov/homeoffice/drt/analytics/prediction/FlightRouteValuesTrainer.scala
+++ src/main/scala/uk/gov/homeoffice/drt/analytics/prediction/FlightRouteValuesTrainer.scala
@@ -157,15 +157,16 @@ case class FlightRouteValuesTrainer(modelName: String,
   }
 
   private def removeOutliers(dataFrame: Dataset[Row]): Dataset[Row] = {
-    dataFrame.stat.approxQuantile("label", Array(0.25, 0.75), 0.0) match {
-      case quantiles if quantiles.length == 2 =>
-        val q1 = quantiles(0)
-        val q3 = quantiles(1)
-        val iqr = q3 - q1
-        val lowerRange = q1 - 1.5 * iqr
-        val upperRange = q3 + 1.5 * iqr
-        dataFrame.filter(s"$lowerRange <= label and label <= $upperRange")
-      case _ => dataFrame
-    }
+//    dataFrame.stat.approxQuantile("label", Array(0.25, 0.75), 0.0) match {
+//      case quantiles if quantiles.length == 2 =>
+//        val q1 = quantiles(0)
+//        val q3 = quantiles(1)
+//        val iqr = q3 - q1
+//        val lowerRange = q1 - 1.5 * iqr
+//        val upperRange = q3 + 1.5 * iqr
+//        dataFrame.filter(s"$lowerRange <= label and label <= $upperRange")
+//      case _ => dataFrame
+//    }
+    dataFrame
   }
 }
